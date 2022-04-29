0ec8047f5db217c2dc1de6e6079a370c98416ac4
Ashwin746544 <ahpatel99999@gmail.com>
2022-04-29 16:21:03 -0700

    feat: changes some styling and logic

--- src/App.js
+++ src/App.js
@@ -33,8 +33,20 @@ const App = () => {
 
   const copyPatternHanlder = () => {
     setCopied(true);
+    let data = ref.current.innerText;
+    data = data.replace(/\u2B1C/g, "      ");
+    // data.replace('   ', '      ');
     console.log("text", ref.current.innerText);
-    navigator.clipboard.writeText(ref.current.innerText);
+    navigator.clipboard.writeText(data);
+    // navigator.clipboard.writeText(data);
+  }
+
+  const getTransformedNum = (n) => {
+    if (n < 1e4) return n;
+    if (n >= 1e4 && n < 1e6) return +(n / 1e3).toFixed(0) + "K";
+    if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(0) + "M";
+    if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(0) + "B";
+    if (n >= 1e12) return +(n / 1e12).toFixed(0) + "T";
   }
 
   const generateEmojiPatternHandler = (event) => {
@@ -50,8 +62,20 @@ const App = () => {
       alert("please enter text!");
       return;
     }
+
+    const transformedInput = getTransformedNum(userInput);
+    console.log("transformedInput", transformedInput);
+
     let mergedArray = null;
-    [...userInput].forEach((char, index) => {
+    // [...userInput].forEach((char, index) => {
+    //   const currentBitPattent = characterBits[char];
+    //   if (!mergedArray) {
+    //     mergedArray = currentBitPattent;
+    //   } else {
+    //     mergedArray = mergedArray.map((bits, index) => bits.concat([0, ...currentBitPattent[index]]));
+    //   }
+    // });
+    [...transformedInput].forEach((char, index) => {
       const currentBitPattent = characterBits[char];
       if (!mergedArray) {
         mergedArray = currentBitPattent;
@@ -63,47 +87,54 @@ const App = () => {
     let actualPattern = "";
     mergedArray.forEach(bits => {
       bits.forEach(bit => actualPattern += (bit === 1 ? chosenEmoji.emoji : (bit === 0 ? "⬜" : bit)))
+      // bits.forEach(bit => actualPattern += (bit === 1 ? chosenEmoji.emoji : (bit === 0 ? "ㅤ " : bit)))
     });
     console.log(actualPattern);
     setEmojiPattern(actualPattern);
   }
 
   return (
-    <div className='App'>
-      <h1>Emoji Pattern Generator</h1>
-      <Form onSubmit={generateEmojiPatternHandler} className="d-flex flex-column align-items-center">
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label className='me'><strong>Enter Text</strong></Form.Label>
-          <Form.Control as="textarea" rows={3} placeholder="text..." onChange={onchangeHandler} value={userInput} style={{ resize: "none" }} />
-        </Form.Group>
-        <Button variant='primary' type='submit'>Generate Pattern</Button>
-      </Form>
-      <h3>Select Emoji</h3>
-      <Picker onEmojiClick={onEmojiClick} />
-      {chosenEmoji ? (
-        <span className='chosenEmoji'><strong>You chose:</strong> {chosenEmoji.emoji}</span>
-      ) : (
-        <span>No emoji Chosen</span>
-      )}
-      {emojiPattern && (
-        <Card>
-          <Card.Header className='d-flex justify-content-between'>
-            <h3 className='m-0'>Pattern :</h3>
-            <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{copied ? "Already Copied" : "Copy Pattern"}</Tooltip>}>
-              <span className="d-inline-block">
-                <Button onClick={copyPatternHanlder} className='m-0'>
-                  {copied ? "Copied" : "Copy"}
-                </Button>
-              </span>
-            </OverlayTrigger>
-          </Card.Header>
-          <Card.Body>
-            <div>
-              <pre className='output' id="pattern-container" ref={ref}>{emojiPattern}</pre>
-            </div>
-          </Card.Body>
-        </Card>
-      )}
+    <div className='App d-flex flex-column align-items-center'>
+      <div className='title-container mt-3'>
+        <h1>Emoji Pattern Generator</h1>
+      </div>
+      <div className='content-container d-flex flex-row mt-5 align-items-start justify-content-between' style={{ gridGap: "50px" }}>
+        <Form onSubmit={generateEmojiPatternHandler} className="d-flex flex-column align-items-start text-container">
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label className='me'><strong>Enter Text</strong></Form.Label>
+            <Form.Control as="textarea" rows={3} placeholder="text..." onChange={onchangeHandler} value={userInput} style={{ resize: "none" }} />
+          </Form.Group>
+          <Button variant='primary' type='submit'>Generate Pattern</Button>
+        </Form>
+        <div className='emojiPicker-container'>
+          <h3>Select Emoji</h3>
+          <Picker onEmojiClick={onEmojiClick} />
+          {chosenEmoji ? (
+            <span className='chosenEmoji mt-3 d-inline-block text-center'><strong>You chose:</strong> {chosenEmoji.emoji}</span>
+          ) : (
+            <span>No emoji Chosen</span>
+          )}
+        </div>
+        {emojiPattern && (
+          <Card className='output-container'>
+            <Card.Header className='d-flex justify-content-between'>
+              <h3 className='m-0'>Pattern :</h3>
+              <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{copied ? "Already Copied" : "Copy Pattern"}</Tooltip>}>
+                <span className="d-inline-block">
+                  <Button onClick={copyPatternHanlder} className='m-0'>
+                    {copied ? "Copied" : "Copy"}
+                  </Button>
+                </span>
+              </OverlayTrigger>
+            </Card.Header>
+            <Card.Body>
+              <div>
+                <pre className='output' id="pattern-container" ref={ref}>{emojiPattern}</pre>
+              </div>
+            </Card.Body>
+          </Card>
+        )}
+      </div>
     </div>
   );
 };
